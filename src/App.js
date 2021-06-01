2b65ed8cf4415c385512421c00ff4c3ea8f3c8f7
Sumitra-1810 <sumitrathawani899@gmail.com>
2021-06-01 11:25:01 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Redirect, Route, Switch } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
-import Contact from "./Contact";
+
 import Navbar from "./Navbar";
 import "../src/Css/App.css";
 
@@ -18,8 +18,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/services" component={Services} />
-          <Route exact path="/contact" component={Contact} />
-          <Redirect to="/" />
+           <Redirect to="/" />
         </Switch>
         <About />
         <Services />
